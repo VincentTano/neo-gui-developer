1d181f3054b1090209b02ccf17b8bc68a868b52c
VincentTano <VincentTano@users.noreply.github.com>
2017-08-14 18:03:22 +1000

    Tidy up InvokeContractDialog.cs
    SignatureContext is not required to get Witness

--- neo-gui/UI/InvokeContractDialog.cs
+++ neo-gui/UI/InvokeContractDialog.cs
@@ -10,7 +10,6 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Windows.Forms;
-using Neo.Wallets;
 
 namespace Neo.UI
 {
@@ -146,28 +145,11 @@ namespace Neo.UI
             StateMachine service = new StateMachine(accounts, validators, assets, contracts, storages);
 
             ////////////////////////////////////////////////////////////
-            ////////////////////WARNING!!!!!!!!!!!!!////////////////////
-            //////////THIS MIGHT MAKE TEST INVOCATION LESS SECURE///////
-            //////////STRICTLY FOR TESTING PURPOSES ONLY////////////////
-            ////////////////////////////////////////////////////////////
+            /////////////////////EXPERIMENTAL///////////////////////////
             testTx = tx;
-            testTx.Gas = Fixed8.One;
+            testTx.Gas = Fixed8.Satoshi;
             testTx = GetTransaction();
-            SignatureContext context;
-            try
-            {
-                context = new SignatureContext(testTx);
-            }
-            catch (InvalidOperationException)
-            {
-                MessageBox.Show(Strings.UnsynchronizedBlock);
-                return;
-            }
-            Program.CurrentWallet.Sign(context);
-            context.Verifiable.Scripts = context.GetScripts();
-            testTx.Scripts = context.Verifiable.Scripts;
-            ////////////////////////////////////////////////////////////
-            ////////////////////WARNING!!!!!!!!!!!!!////////////////////
+            /////////////////////EXPERIMENTAL///////////////////////////            
             ////////////////////////////////////////////////////////////
 
 
